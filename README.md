17d85b2ff0780d17e5ce18769b7ac7232c352aaa
Maxim Temryakov <therevors@gmail.com>
2023-05-25 23:08:51 +0300

    Created folder structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # snippetapp
+
+That repository exists for practice with Go. Currently I learn how to build clean, supportable architecture.
--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,62 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+type Snippet struct {
+	ID          string
+	Name        string
+	Description string
+	Author      string
+}
+
+func getSnippetListController(c *gin.Context) {
+
+	s1 := Snippet{"1", "It's my snippet", "I've just created first snippet in that app!", "Maxim"}
+	s2 := Snippet{"2", "Quality", "Quality is not an act, it is a habit.", "Aristotle"}
+	s3 := Snippet{"3", "Safety", "Safety is important", "Anonymous"}
+
+	array := []Snippet{s1, s2, s3}
+
+	c.JSON(http.StatusOK, array)
+}
+
+func getSnippetController(c *gin.Context) {
+
+	s1 := Snippet{"1", "It's my snippet", "I've just created first snippet in that app!", "Maxim"}
+	s2 := Snippet{"2", "Quality", "Quality is not an act, it is a habit.", "Aristotle"}
+	s3 := Snippet{"3", "Safety", "Safety is important", "Anonymous"}
+
+	array := []Snippet{s1, s2, s3}
+
+	id := c.Param("id")
+	found := false
+
+	for _, snippet := range array {
+		if id == snippet.ID {
+			c.JSON(http.StatusOK, snippet)
+			found = true
+			break
+		}
+	}
+	if !found {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Not found"})
+	}
+}
+
+func helloController(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello, it's my snippet application!",
+	})
+}
+
 func main() {
 	router := gin.Default()
-	// router.GET("/", getListController.getList)
-	// router.GET("/snippets/:id", getSnippet)
-	// router.POST("/snippet", postSnippet)
-
-	// log.Println("Run server on", env.serverAddress)
-
+	router.GET("/api", helloController)
+	router.GET("/api/snippet", getSnippetListController)
+	router.GET("/api/snippet/:id", getSnippetController)
 	router.Run("localhost:8080")
 }
