2e0cad3d2af412ca6cd398d8ce79243b11af94dc
Maxim Temryakov <therevors@gmail.com>
2023-06-22 00:30:58 +0300

    Remove legacy code from main.go

--- cmd/main.go
+++ cmd/main.go
@@ -6,14 +6,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
-
-type Snippet struct {
-	Title string
-	Text  string
-	gorm.Model
-}
 
 func main() {
 	app := bootstrap.App()
@@ -23,26 +16,6 @@ func main() {
 	gin := gin.Default()
 
 	route.Setup(config, db, timeout, gin)
+
 	gin.Run(config.ServerAddress)
 }
-
-// func getSnippetByID(db *gorm.DB, id string) (Snippet, error) {
-// 	var snippet Snippet
-
-// 	err := db.First(&snippet, id).Error
-
-// 	if err != nil {
-// 		return snippet, errors.New("Snippet not found")
-// 	}
-
-// 	return snippet, nil
-// }
-
-// func getSnippetController(c *gin.Context) {
-// 	snippet, err := getSnippetByID(db, c.Param("id"))
-// 	if err != nil {
-// 		c.JSON(http.StatusNotFound, gin.H{"error": "Snippet not found"})
-// 		return
-// 	}
-// 	c.JSON(http.StatusOK, snippet)
-// }
