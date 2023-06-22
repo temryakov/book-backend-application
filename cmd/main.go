71deda70cac1b3e0d45609ba28ddbb415a007619
Maxim Temryakov <therevors@gmail.com>
2023-06-23 00:01:24 +0300

    Added comments to main.go

--- cmd/main.go
+++ cmd/main.go
@@ -10,8 +10,13 @@ import (
 
 func main() {
 	app := bootstrap.App()
+
+	/* Enviroment variables initializing */
 	config := app.Config
+
+	/* Database connecting initializing */
 	db := app.DB
+
 	timeout := time.Duration(config.ContextTimeout) * time.Second
 	gin := gin.Default()
 
