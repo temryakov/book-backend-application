7cf1acced47a255e94a9c0ac10e2d7fa1d1ff724
Maxim Temryakov <therevors@gmail.com>
2023-07-07 21:49:52 +0300

    Changed SignupRequest struct

--- user-service/domain/signup.go
+++ user-service/domain/signup.go
@@ -1,9 +1,9 @@
 package domain
 
 type SignupRequest struct {
-	Name     string `form:"name" binding:"required"`
-	Email    string `form:"email" binding:"required,email"`
-	Password string `form:"password" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Name     string `json:"name" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 type SignupResponse struct {
