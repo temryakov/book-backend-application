2cd7a4626f80713b8c8e24085a292a113dc66dc0
Maxim Temryakov <therevors@gmail.com>
2023-07-08 15:13:16 +0300

    Implemented sign up usecase

--- user-service/domain/signup.go
+++ user-service/domain/signup.go
@@ -1,16 +1,13 @@
 package domain
 
+import "context"
+
 type SignupRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Name     string `json:"name" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
-type SignupResponse struct {
-	AccessToken  string `json:"accessToken"`
-	RefreshToken string `json:"refreshToken"`
+type SignupUsecase interface {
+	Create(c context.Context, user *User) error
 }
-
-// type SignupUsecase interface {
-// 	Create(c context.Context, user *User) error
-// }
---
+++ user-service/usecase/signup_usecase.go
@@ -0,0 +1,26 @@
+package usecase
+
+import (
+	"context"
+	"time"
+
+	"github.com/temryakov/go-backend-book-app/user-service/domain"
+)
+
+type signupUsecase struct {
+	userRepository domain.UserRepository
+	contextTimeout time.Duration
+}
+
+func NewSignupUsecase(userRepository domain.UserRepository, timeout time.Duration) domain.UserRepository {
+	return &signupUsecase{
+		userRepository: userRepository,
+		contextTimeout: timeout,
+	}
+}
+
+func (su *signupUsecase) Create(c context.Context, user *domain.User) error {
+	ctx, cancel := context.WithTimeout(c, su.contextTimeout)
+	defer cancel()
+	return su.userRepository.Create(ctx, user)
+}
