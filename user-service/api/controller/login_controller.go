6466032fc0da6ce32b10e504c4875c1018c8c032
Maxim Temryakov <therevors@gmail.com>
2023-07-15 11:30:04 +0300

    Implemented access and refresh tokens fetching

--- user-service/api/controller/login_controller.go
+++ user-service/api/controller/login_controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/temryakov/go-backend-book-app/user-service/bootstrap"
 	"github.com/temryakov/go-backend-book-app/user-service/domain"
+	"github.com/temryakov/go-backend-book-app/user-service/tokenutil"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -18,7 +19,7 @@ func (u *LoginController) Login(c *gin.Context) {
 	var request *domain.LoginRequest
 
 	if err := c.ShouldBind(&request); err != nil {
-		c.JSON(http.StatusBadRequest, domain.MessageBadRequest)
+		c.JSON(http.StatusBadRequest, domain.ErrBadRequest)
 		return
 	}
 	user, err := u.LoginUsecase.FetchByEmail(c, request.Email)
@@ -29,8 +30,20 @@ func (u *LoginController) Login(c *gin.Context) {
 		return
 	}
 	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password)) != nil {
-		c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "Invalid credentials"})
+		c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "Invalid credentials %/"})
 		return
 	}
-	c.JSON(http.StatusOK, request)
+	accessToken, err := tokenutil.CreateAccessToken(user, u.Config.SecretKey, u.Config.ExpiryHours)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, domain.ErrInternalServerError)
+	}
+	refreshToken, err := tokenutil.CreateRefreshToken(user, u.Config.SecretKey, u.Config.ExpiryHours)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, domain.ErrInternalServerError)
+	}
+
+	c.JSON(http.StatusOK, domain.LoginResponse{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	})
 }
--- user-service/tokenutil/tokenutil.go
+++ user-service/tokenutil/tokenutil.go
@@ -1,9 +1,40 @@
 package tokenutil
 
-func CreateAccessToken() {
+import (
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/temryakov/go-backend-book-app/user-service/domain"
+)
+
+func CreateAccessToken(user *domain.User, secret string, expiry int) (string, error) {
+	exp := &jwt.NumericDate{Time: time.Now().Add(time.Hour * time.Duration(expiry))}
+	claims := &domain.JwtCustomClaims{
+		Name: user.Name,
+		ID:   string(rune(user.Model.ID)),
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: exp,
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+	return t, err
 }
 
-func RefreshAccessToken() {
-
+func CreateRefreshToken(user *domain.User, secret string, expiry int) (string, error) {
+	claimsRefresh := &domain.JwtCustomRefreshClaims{
+		ID: string(rune(user.Model.ID)),
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: &jwt.NumericDate{Time: time.Now().Add(time.Hour * time.Duration(expiry))},
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claimsRefresh)
+	rt, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+	return rt, err
 }
