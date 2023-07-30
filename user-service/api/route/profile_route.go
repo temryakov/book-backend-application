7fb5b2399c11a86af776b0f4c4347ea7c7305edd
Maxim Temryakov <therevors@gmail.com>
2023-07-30 12:30:58 +0300

    Implemented profile endpoint

---
+++ user-service/api/route/profile_route.go
@@ -0,0 +1,20 @@
+package route
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/temryakov/go-backend-book-app/user-service/api/controller"
+	"github.com/temryakov/go-backend-book-app/user-service/bootstrap"
+	"github.com/temryakov/go-backend-book-app/user-service/repository"
+	"github.com/temryakov/go-backend-book-app/user-service/usecase"
+	"gorm.io/gorm"
+)
+
+func NewProfileRouter(config *bootstrap.Config, timeout time.Duration, db *gorm.DB, group *gin.RouterGroup) {
+	ur := repository.NewUserRepository(db)
+	sc := &controller.ProfileController{
+		ProfileUsecase: usecase.NewProfileUsecase(ur, timeout),
+	}
+	group.GET("/profile", sc.Create)
+}
