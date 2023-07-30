673227c3089d727d2a20b9c86cb76fa6d0f36da8
Maxim Temryakov <therevors@gmail.com>
2023-07-30 12:31:55 +0300

    Implemented token verification and id extracting from token

--- user-service/tokenutil/tokenutil.go
+++ user-service/tokenutil/tokenutil.go
@@ -1,6 +1,7 @@
 package tokenutil
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -37,4 +38,38 @@ func CreateRefreshToken(user *domain.User, secret string, expiry int) (string, e
 		return "", err
 	}
 	return rt, err
+}
+
+func IsAuthorized(requestToken string, secret string) (bool, error) {
+	_, err := jwt.Parse(requestToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func ExtractIDFromToken(requestToken string, secret string) (string, error) {
+	token, err := jwt.Parse(requestToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+
+	if !ok && !token.Valid {
+		return "", domain.ErrInvalidToken
+	}
+
+	return claims["id"].(string), nil
 }
