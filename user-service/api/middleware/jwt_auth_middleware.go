df71094a4fe1513abf44ba8f501ff8576c0c07b4
Maxim Temryakov <therevors@gmail.com>
2023-07-31 10:14:25 +0300

    Implemented jwt middleware

---
+++ user-service/api/middleware/jwt_auth_middleware.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/temryakov/go-backend-book-app/user-service/domain"
+	"github.com/temryakov/go-backend-book-app/user-service/tokenutil"
+)
+
+func AuthMiddleware(secret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("Authorization")
+		t := strings.Split(authHeader, " ")
+		if len(t) == 2 {
+			authToken := t[1]
+			authorized, err := tokenutil.IsAuthorized(authToken, secret)
+			if authorized {
+				userId, err := tokenutil.ExtractIDFromToken(authToken, secret)
+				if err != nil {
+					c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: err.Error()})
+					c.Abort()
+					return
+				}
+				c.Set("x-user-id", userId)
+				c.Next()
+				return
+			}
+			c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: err.Error()})
+			c.Abort()
+			return
+		}
+		c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "Not authorized =("})
+		c.Abort()
+	}
+}
--- user-service/api/route/route.go
+++ user-service/api/route/route.go
@@ -4,14 +4,20 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/temryakov/go-backend-book-app/user-service/api/middleware"
 	"github.com/temryakov/go-backend-book-app/user-service/bootstrap"
 	"gorm.io/gorm"
 )
 
 func Setup(config *bootstrap.Config, timeout time.Duration, db *gorm.DB, gin *gin.Engine) {
-	publicRouter := gin.Group("/api")
 
 	// Public APIs
+	publicRouter := gin.Group("/api")
 	NewSignupRouter(config, timeout, db, publicRouter)
 	NewLoginRouter(config, timeout, db, publicRouter)
+
+	// Protected APIs
+	protectedRouter := gin.Group("/api")
+	protectedRouter.Use(middleware.AuthMiddleware(config.SecretKey))
+	NewProfileRouter(config, timeout, db, protectedRouter)
 }
