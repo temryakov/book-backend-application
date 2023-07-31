bdf5eba9165818921601b28c81a9b4a96fbffd19
Maxim Temryakov <therevors@gmail.com>
2023-07-31 22:20:44 +0300

    Added enviroment variables

---
+++ review-service/bootstrap/config.go
@@ -0,0 +1,39 @@
+package bootstrap
+
+import (
+	"log"
+	"sync"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	ServerAddress  string `mapstructure:"SERVER_ADDRESS"`
+	ContextTimeout int    `mapstructure:"CONTEXT_TIMEOUT"`
+	DBHost         string `mapstructure:"DB_HOST"`
+	DBPort         string `mapstructure:"DB_PORT"`
+	DBUser         string `mapstructure:"DB_USER"`
+	DBName         string `mapstructure:"DB_NAME"`
+}
+
+var (
+	config Config
+	once   sync.Once
+)
+
+func Get() *Config {
+	config := Config{}
+	viper.SetConfigFile(".env")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal("Can't find the file .env : ", err)
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		log.Fatal("Environment can't be loaded: ", err)
+	}
+
+	return &config
+}
