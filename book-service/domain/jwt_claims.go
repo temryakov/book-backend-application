e3b1a5f6286d9ff14870e045e8b0143f1aa902d6
Maxim Temryakov <therevors@gmail.com>
2023-07-31 21:53:54 +0300

    Implemented jwt authentication

---
+++ book-service/domain/jwt_claims.go
@@ -0,0 +1,17 @@
+package domain
+
+import (
+	"errors"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var (
+	ErrInvalidToken = errors.New("invalid token")
+)
+
+type JwtCustomClaims struct {
+	Name string `json:"name"`
+	ID   uint   `json:"id"`
+	jwt.RegisteredClaims
+}
