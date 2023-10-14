3ffc29eede496705863bfbadb7fe76175151638b
Maxim Temryakov <therevors@gmail.com>
2023-10-15 00:13:57 +0300

    Added logs

--- book-service/grpc_server/main.go
+++ book-service/grpc_server/main.go
@@ -26,6 +26,7 @@ import (
 	"log"
 	"net"
 
+	"github.com/temryakov/go-backend-book-app/book-service/domain"
 	pb "github.com/temryakov/go-backend-book-app/book-service/proto"
 	"google.golang.org/grpc"
 )
@@ -34,29 +35,47 @@ var (
 	port = flag.Int("port", 50051, "The server port")
 )
 
-// server is used to implement helloworld.GreeterServer.
 type server struct {
 	pb.UnimplementedBookServiceServer
+	repository domain.BookRepository
 }
 
-// SayHello implements helloworld.GreeterServer
 func (s *server) GetBook(ctx context.Context, in *pb.GetBookRequest) (*pb.GetBookResponse, error) {
-	log.Printf("Received: %v", in.GetId())
+
+	log.Printf("GetBook triggered\n")
+
+	id := in.GetId()
+	log.Printf("Received: %v", id)
+
+	log.Printf("Fetch book through its repository")
+
+	book, err := s.repository.FetchBookByID(ctx, uint(id))
+
+	if err != nil {
+		return nil, err
+	}
+
 	return &pb.GetBookResponse{
-		Title:  "gRPC Test Title",
-		Author: "gRPC Test Author",
+		Title:  book.Title,
+		Author: book.Author,
 	}, nil
 }
 
 func main() {
 	flag.Parse()
+
+	log.Printf("gRPC server is started")
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("Failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterBookServiceServer(s, &server{})
-	log.Printf("server listening at %v", lis.Addr())
+
+	log.Printf("Server listening at %v", lis.Addr())
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
