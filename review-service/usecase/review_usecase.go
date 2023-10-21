466217b1752560339bca4e5e69be3a42fd70d43b
Maxim Temryakov <therevors@gmail.com>
2023-10-22 02:46:52 +0300

    Added request sending to another service

--- review-service/usecase/review_usecase.go
+++ review-service/usecase/review_usecase.go
@@ -2,6 +2,11 @@ package usecase
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/review-service/domain"
@@ -23,7 +28,42 @@ func NewReviewUsecase(reviewRepository domain.ReviewRepository, timeout time.Dur
 func (ru *reviewUsecase) FetchReview(c context.Context, conditions *domain.ReviewQuery) (*domain.Review, error) {
 	ctx, cancel := context.WithTimeout(c, ru.contextTimeout)
 	defer cancel()
-	return ru.reviewRepository.FetchReview(ctx, conditions)
+
+	review, err := ru.reviewRepository.FetchReview(ctx, conditions)
+
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+
+	url := "http://localhost:8080/api/book/" + strconv.FormatUint(uint64(review.BookId), 10)
+	req, _ := http.NewRequest("GET", url, nil)
+
+	log.Print(url)
+
+	req.Header.Set("Accept", "application/x-protobuf")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("Error making HTTP request:", err)
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return nil, err
+	}
+
+	log.Print(body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return review, nil
 }
 
 func (ru *reviewUsecase) CreateReview(c context.Context, review *domain.Review) error {
