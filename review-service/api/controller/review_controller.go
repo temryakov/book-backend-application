659ebb0c6a30ac240627859b166691f9027d5885
Maxim Temryakov <therevors@gmail.com>
2023-10-22 03:46:37 +0300

    Commented improperly written controller

--- review-service/api/controller/review_controller.go
+++ review-service/api/controller/review_controller.go
@@ -38,64 +38,57 @@ func (u *ReviewController) FetchReview(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, domain.ReviewResponse{
-		BookAuthor:   "Book Author",
-		BookTitle:    "Book Title",
-		ReviewAuthor: "Review Author",
-		Rating:       review.Rating,
-		ReviewTitle:  review.Title,
-		ReviewText:   review.Text,
-	})
+	c.JSON(http.StatusOK, review)
 }
 
-func (u *ReviewController) DeleteReview(c *gin.Context) {
+// func (u *ReviewController) DeleteReview(c *gin.Context) {
 
-	reviewId, err := strconv.ParseUint(c.Param("id"), 0, 16)
+// 	reviewId, err := strconv.ParseUint(c.Param("id"), 0, 16)
 
-	userId := c.GetUint("x-user-id")
+// 	userId := c.GetUint("x-user-id")
 
-	if err != nil {
-		c.JSON(http.StatusBadRequest, domain.MessageBadRequest)
-		return
-	}
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, domain.MessageBadRequest)
+// 		return
+// 	}
 
-	query := domain.ReviewQuery{
-		ID: uint(reviewId),
-	}
+// 	query := domain.ReviewQuery{
+// 		ID: uint(reviewId),
+// 	}
 
-	review, err := u.ReviewUsecase.FetchReview(c, &query)
+// 	review, err := u.ReviewUsecase.FetchReview(c, &query)
 
-	if err == gorm.ErrRecordNotFound {
-		c.JSON(http.StatusNotFound, domain.ReviewNotFound)
-		return
-	}
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, domain.MessageInternalServerError)
-		return
-	}
+// 	if err == gorm.ErrRecordNotFound {
+// 		c.JSON(http.StatusNotFound, domain.ReviewNotFound)
+// 		return
+// 	}
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, domain.MessageInternalServerError)
+// 		return
+// 	}
 
-	if userId != review.UserId {
-		c.JSON(http.StatusForbidden, domain.ErrorResponse{
-			Message: "You can not delete this review.",
-		})
-		return
-	}
+// 	if userId != review.UserId {
+// 		c.JSON(http.StatusForbidden, domain.ErrorResponse{
+// 			Message: "You can not delete this review.",
+// 		})
+// 		return
+// 	}
 
-	err = u.ReviewUsecase.DeleteReview(c, uint(reviewId))
+// 	err = u.ReviewUsecase.DeleteReview(c, uint(reviewId))
 
-	if err == gorm.ErrRecordNotFound {
-		c.JSON(http.StatusNotFound, domain.ReviewNotFound)
-		return
-	}
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, domain.MessageInternalServerError)
-		return
-	}
+// 	if err == gorm.ErrRecordNotFound {
+// 		c.JSON(http.StatusNotFound, domain.ReviewNotFound)
+// 		return
+// 	}
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, domain.MessageInternalServerError)
+// 		return
+// 	}
 
-	c.JSON(http.StatusOK, domain.SuccessfulMessage{
-		Message: "Review successfully deleted",
-	})
-}
+// 	c.JSON(http.StatusOK, domain.SuccessfulMessage{
+// 		Message: "Review successfully deleted",
+// 	})
+// }
 
 func (u *ReviewController) CreateReview(c *gin.Context) {
 
