9152fdcb901cf8383ecb6044f6e4a7bab3d72627
Maxim Temryakov <therevors@gmail.com>
2023-11-26 20:57:39 +0300

    Fixed password setting for database connection & added logs

--- review-service/bootstrap/database.go
+++ review-service/bootstrap/database.go
@@ -10,11 +10,14 @@ import (
 
 func Psql(cfg *Config) *gorm.DB {
 
-	var dsn = fmt.Sprintf("host=%s user=%s password=abc123 dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", cfg.DBHost, cfg.DBUser, cfg.DBName, cfg.DBPort)
+	var dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort)
+
+	fmt.Printf("\nConfiguring database settings: \n Host: %s\n Port: %s\n Database Name: %s\n User: %s", cfg.DBHost, cfg.DBPort, cfg.DBName, cfg.DBUser)
+
 	client, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		log.Fatal("Internal database error")
+		log.Fatal("Failed to connect to PostgreSQL Database")
 	}
 	return client
 }
