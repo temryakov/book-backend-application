aa529f3bc7f2ca7afda2564c76b96493690e3340
Maxim Temryakov <therevors@gmail.com>
2023-11-27 10:18:18 +0300

    Added logs & GORM migration in user-service

--- user-service/bootstrap/database.go
+++ user-service/bootstrap/database.go
@@ -4,17 +4,28 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/temryakov/go-backend-book-app/user-service/domain"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func Psql(cfg *Config) *gorm.DB {
 
-	var dsn = fmt.Sprintf("host=%s user=%s password=abc123 dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", cfg.DBHost, cfg.DBUser, cfg.DBName, cfg.DBPort)
+	var dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort)
+
+	fmt.Printf("\nConfiguring database settings: \n Host: %s\n Port: %s\n Database Name: %s\n User: %s", cfg.DBHost, cfg.DBPort, cfg.DBName, cfg.DBUser)
+
 	client, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		log.Fatal("Internal database error")
+		log.Fatal("Failed to connect to PostgreSQL Database")
 	}
+	log.Println("Connection to database is successfully")
+
+	client.Logger = logger.Default.LogMode(logger.Info)
+
+	client.AutoMigrate(&domain.User{})
+
 	return client
 }
