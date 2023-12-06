7e24d5d5d0a6745d373640269b511f74b2ab4346
Maxim Temryakov <therevors@gmail.com>
2023-12-06 23:51:16 +0300

    Added not enough permissions error in review service

--- review-service/domain/errors.go
+++ review-service/domain/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrReviewNotFound = errors.New("review not found")
 	ErrReviewIsExist  = errors.New("review is exist")
+	ErrNotPermitted   = errors.New("not enough permissions")
 )
