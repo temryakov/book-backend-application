22ad32f743e413657b5c66d0becc33d7f766677e
Maxim Temryakov <therevors@gmail.com>
2024-02-08 10:34:55 +0300

    Renamed interface calling

--- book-service/usecase/book_usecase.go
+++ book-service/usecase/book_usecase.go
@@ -19,31 +19,31 @@ func NewBookUsecase(bookRepository domain.BookRepository, timeout time.Duration)
 	}
 }
 
-func (su *bookUsecase) FetchBookByID(c context.Context, bookId int) (*domain.Book, error) {
-	ctx, cancel := context.WithTimeout(c, su.contextTimeout)
+func (bu *bookUsecase) FetchBookByID(c context.Context, bookId int) (*domain.Book, error) {
+	ctx, cancel := context.WithTimeout(c, bu.contextTimeout)
 	defer cancel()
-	return su.bookRepository.FetchBookByID(ctx, bookId)
+	return bu.bookRepository.FetchBookByID(ctx, bookId)
 }
 
-func (su *bookUsecase) FetchBooks(c context.Context) (*[]domain.Book, error) {
-	ctx, cancel := context.WithTimeout(c, su.contextTimeout)
+func (bu *bookUsecase) FetchBooks(c context.Context) (*[]domain.Book, error) {
+	ctx, cancel := context.WithTimeout(c, bu.contextTimeout)
 	defer cancel()
-	return su.bookRepository.FetchBooks(ctx)
+	return bu.bookRepository.FetchBooks(ctx)
 }
 
-func (su *bookUsecase) CreateBook(c context.Context, book *domain.Book) error {
-	ctx, cancel := context.WithTimeout(c, su.contextTimeout)
+func (bu *bookUsecase) CreateBook(c context.Context, book *domain.Book) error {
+	ctx, cancel := context.WithTimeout(c, bu.contextTimeout)
 	defer cancel()
-	return su.bookRepository.CreateBook(ctx, book)
+	return bu.bookRepository.CreateBook(ctx, book)
 }
-func (su *bookUsecase) UpdateBook(c context.Context, book *domain.Book, bookId int) error {
-	ctx, cancel := context.WithTimeout(c, su.contextTimeout)
+func (bu *bookUsecase) UpdateBook(c context.Context, book *domain.Book, bookId int) error {
+	ctx, cancel := context.WithTimeout(c, bu.contextTimeout)
 	defer cancel()
-	model, err := su.bookRepository.FetchBookByID(ctx, bookId)
+	model, err := bu.bookRepository.FetchBookByID(ctx, bookId)
 	if err != nil {
 		return err
 	}
-	return su.bookRepository.UpdateBook(ctx, book, model)
+	return bu.bookRepository.UpdateBook(ctx, book, model)
 }
 
 func (su *bookUsecase) DeleteBook(c context.Context, bookId int) error {
