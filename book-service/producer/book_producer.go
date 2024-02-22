92ddd5aec77e20d99e2930db698b426c6b5ceffa
Maxim Temryakov <therevors@gmail.com>
2024-02-22 16:17:06 +0300

    Added book producer

---
+++ book-service/producer/book_producer.go
@@ -0,0 +1,29 @@
+package kafka
+
+import (
+	"book-service/domain"
+	"context"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/segmentio/kafka-go"
+)
+
+type bookProducer struct {
+	producer *kafka.Producer
+	topic    string
+}
+
+func NewBookProducer(producer *kafka.Producer) domain.BookProducer {
+	return &bookProducer{
+		producer: producer,
+		topic:    "books",
+	}
+}
+
+func (p *bookProducer) DeleteBook(ctx context.Context, id string) {
+	p.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &p.topic, Partition: kafka.PartitionAny},
+		Key:            []byte(id),
+		Value:          []byte("delete"),
+	}, nil)
+}
