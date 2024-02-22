925aa040e6b73383a7ba6e0205834a2de9001dbf
Maxim Temryakov <therevors@gmail.com>
2024-02-22 16:43:04 +0300

    Refactored book_producer

--- book-service/producer/book_producer.go
+++ book-service/producer/book_producer.go
@@ -2,10 +2,8 @@ package prod
 
 import (
 	"book-service/domain"
-	"context"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"github.com/segmentio/kafka-go"
 )
 
 type bookProducer struct {
@@ -13,17 +11,21 @@ type bookProducer struct {
 	topic    string
 }
 
-func NewBookProducer(producer *kafka.Producer) domain.BookProducer {
+func NewBookProducer(producer *kafka.Producer, id string, data string) domain.BookProducer {
 	return &bookProducer{
 		producer: producer,
 		topic:    "books",
 	}
 }
 
-func (p *bookProducer) DeleteBook(ctx context.Context, id string) {
-	p.producer.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &p.topic, Partition: kafka.PartitionAny},
+func (bp *bookProducer) WriteMessage(id string, data string) {
+	bp.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &bp.topic, Partition: kafka.PartitionAny},
 		Key:            []byte(id),
-		Value:          []byte("delete"),
+		Value:          []byte(data),
 	}, nil)
+}
+
+func (bp *bookProducer) DeleteBook(bookId string, data string) {
+	bp.WriteMessage(bookId, data)
 }
