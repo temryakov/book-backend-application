b15efc3b7da8b158d628aef1f0c5e52ea7523bb5
Maxim Temryakov <therevors@gmail.com>
2024-02-23 18:01:12 +0300

    Set new kafka variables

--- book-service/bootstrap/config.go
+++ book-service/bootstrap/config.go
@@ -8,13 +8,14 @@ import (
 )
 
 type Config struct {
-	ContextTimeout int    `mapstructure:"CONTEXT_TIMEOUT"`
-	DBHost         string `mapstructure:"POSTGRES_HOST"`
-	DBPort         string `mapstructure:"POSTGRES_PORT"`
-	DBUser         string `mapstructure:"POSTGRES_USER"`
-	DBPassword     string `mapstructure:"POSTGRES_PASSWORD"`
-	DBName         string `mapstructure:"POSTGRES_DB"`
-	ServerPort     string `mapstructure:"SERVER_PORT"`
+	ContextTimeout     int    `mapstructure:"CONTEXT_TIMEOUT"`
+	DBHost             string `mapstructure:"POSTGRES_HOST"`
+	DBPort             string `mapstructure:"POSTGRES_PORT"`
+	DBUser             string `mapstructure:"POSTGRES_USER"`
+	DBPassword         string `mapstructure:"POSTGRES_PASSWORD"`
+	DBName             string `mapstructure:"POSTGRES_DB"`
+	ServerPort         string `mapstructure:"SERVER_PORT"`
+	KafkaServerAddress string `mapstructure:"KAFKA_SERVER_ADDRESS"`
 }
 
 var (
